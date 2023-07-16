fe14e5c73498e03d0086a56fe0b3bff1d9c9d5c3
Igor Soares Fernandes <iigorsf.dev@gmail.com>
2023-07-16 11:33:27 -0300

    Throwing and catching an exception.

--- src/main/java/br/com/banco/controller/AccountController.java
+++ src/main/java/br/com/banco/controller/AccountController.java
@@ -2,16 +2,15 @@ package br.com.banco.controller;
 
 import br.com.banco.domain.account.Account;
 import br.com.banco.domain.account.AccountRepository;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/conta")
 public class AccountController {
@@ -20,9 +19,20 @@ public class AccountController {
     private AccountRepository accountRepository;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Account> getAllTransfer(@PathVariable Integer id) {
-        Account account = accountRepository.findById(id).get();
+    public ResponseEntity<Account> getAccount(@PathVariable Integer id) {
+        Optional<Account> accountOptional = accountRepository.findById(id);
+        if(accountOptional.isEmpty()) {
+            throw new EntityNotFoundException("Conta não encontrada");
+        }
+        var account = accountOptional.get();
+
         return ResponseEntity.ok().body(account);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Account>> getAllAccounts() {
+        List<Account> accountList = accountRepository.findAll();
+        return ResponseEntity.ok().body(accountList);
     }
 
 }
