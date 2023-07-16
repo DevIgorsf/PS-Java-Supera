000e89f8030b5e6ba7b324b4c59333709ea35032
Igor Soares Fernandes <iigorsf.dev@gmail.com>
2023-07-16 16:04:38 -0300

    Total balance.

--- src/main/java/br/com/banco/controller/AccountController.java
+++ src/main/java/br/com/banco/controller/AccountController.java
@@ -2,11 +2,13 @@ package br.com.banco.controller;
 
 import br.com.banco.domain.account.Account;
 import br.com.banco.domain.account.AccountRepository;
+import br.com.banco.domain.transfer.Transfer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +29,19 @@ public class AccountController {
         var account = accountOptional.get();
 
         return ResponseEntity.ok().body(account);
+    }
+
+    @GetMapping("/{id}/saldo")
+    public ResponseEntity<BigDecimal> getBalance(@PathVariable Integer id) {
+        Optional<Account> accountOptional = accountRepository.findById(id);
+        if(accountOptional.isEmpty()) {
+            throw new EntityNotFoundException("Conta não encontrada");
+        }
+        var balance = accountOptional.get().getListTransfer().stream()
+                .map(Transfer::getValue)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);;
+
+        return ResponseEntity.ok().body(balance);
     }
 
     @GetMapping
